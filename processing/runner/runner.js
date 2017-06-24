4a1381f23ce904da5585dc4a4431c4db8ee0f467
Bryan Chiu <chiub1@mcmaster.ca>
2017-06-24 15:19:20 -0400

    runner window fix 219

--- processing/runner/runner.js
+++ processing/runner/runner.js
@@ -8,7 +8,7 @@ var obstCount;
 var score;
 
 function setup() {
-  var canvas = createCanvas(windowWidth, (windowHeight>1800 ? 1200 : windowHeight));
+  var canvas = createCanvas(windowWidth, (windowHeight>1600 ? 1600 : windowHeight));
   //parent.canvas = "sketch-holder";
 
   obstList = [];
