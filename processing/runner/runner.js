7c9d001b86a39cfddfb06f07f37a1f38ce8c8a1f
Bryan Chiu <chiub1@mcmaster.ca>
2017-06-25 15:40:43 -0400

    runner 176/15:40

--- processing/runner/runner.js
+++ processing/runner/runner.js
@@ -8,7 +8,7 @@ var obstCount;
 var score;
 
 function setup() {
-  var canvas = createCanvas(windowWidth, (windowHeight>1200 ? 1200 : windowHeight));
+  var canvas = createCanvas(windowWidth, (windowHeight>1000 ? 1000 : windowHeight));
   //parent.canvas = "sketch-holder";
   //hi
 
